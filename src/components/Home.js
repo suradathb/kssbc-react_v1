092d41ffb2236f7de95a616852ae146a924ef991
NoobToNinja <bondnuy007@me.com>
2022-11-01 11:44:45 +0700

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,9 @@
-import React, { Component } from "react";
+import React from "react";
 import KSSBC from "./../abis/KSSBonusToken.json";
 import Web3 from "web3";
 import Header from "./Header";
 
-class Home extends Component {
+class Home extends React.Component {
   async componentWillMount() {
     await this.loadWeb3();
     await this.loadBlockchainData();
