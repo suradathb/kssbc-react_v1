b388b4a77cb6ef30a73220d5bf08f6fe84f7b056
NoobToNinja <bondnuy007@me.com>
2022-11-01 17:23:10 +0700

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,7 +10,7 @@ class Header extends React.Component {
   }
   
   async loadWeb3() {
-    if (window.web3) {
+    if (window.ethereum) {
       window.web3 = new Web3(window.ethereum);
       // await window.ethereum.enable();
       await window.ethereum.request({
