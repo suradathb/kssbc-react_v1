d655b11ab109d8f0e63363bd35cb7aaf0aa81caf
NoobToNinja <bondnuy007@me.com>
2022-11-01 11:44:14 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './components/Home';
 
 
-class App extends Component {
+class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
