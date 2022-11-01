5d680f970fe81221e39ecc25f841d6b69a1a911b
NoobToNinja <bondnuy007@me.com>
2022-11-01 11:45:40 +0700

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from "react-router-dom";
 
 
-class Footer extends Component {
+class Footer extends React.Component {
   constructor(props) {
     super(props);
   }
